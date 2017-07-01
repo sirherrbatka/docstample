27c7e13a635a8f688f3589c0e1bdef1388585eb5
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-07-01 18:11:02 +0200

    Don't use deeply nested tables.

--- src/protocol/common.lisp
+++ src/protocol/common.lisp
@@ -3,28 +3,20 @@
 
 
 (defmethod query-node ((accumulator categorized-accumulator) type id)
-  (let ((package (symbol-package id)))
-    (when-let ((packages-table (gethash (package-name package)
-                                        (read-documented-elements accumulator))))
-      (when-let ((types-table (gethash type
-                                       packages-table)))
-        (gethash (symbol-name id) types-table)))))
+  (when-let ((types-table (gethash type
+                                   (read-documented-elements accumulator))))
+    (gethash id types-table)))
 
 
 (defmethod accumulate-node ((accumulator categorized-accumulator)
-                            name
+                            id
                             (type fundamental-node)
                             (forms list))
-  (let* ((symbol (if (symbolp name) name (second name)))
-         (package (~> symbol symbol-package package-name))
-         (packages-table (ensure
-                             (gethash package
-                                      (read-documented-elements accumulator))
-                           (make-hash-table :test 'equal)))
-         (types-table (ensure (gethash type packages-table)
+  (let* ((types-table (ensure
+                          (gethash type (read-documented-elements accumulator))
                         (make-hash-table :test 'equal))))
-    (setf (gethash (~> symbol symbol-name) types-table)
-          (make-accumulated-node name type forms))))
+    (setf (gethash id types-table)
+          (make-accumulated-node id type forms))))
 
 
 (defmethod generate-documentation-string ((generator fundamental-generator)
